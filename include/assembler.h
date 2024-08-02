859fb37d2ec1ebb3c4a8c98dc0b53ef6c85d8348
Eden.Kellner <ednkell@gmail.com>
2024-08-02 10:25:38 +0300

    Assembler API

--- include/assembler.h
+++ include/assembler.h
@@ -3,13 +3,20 @@
 
 #include "utils.h"
 
-result_t AssembleFiles(char *files[]);
+/*
+ * @brief Assembler which takes .am files (after preprocessing)
+ *        and converts them to corresponding object files.
+ * @param file_path - Path to the .am file to assemble.
+ * @return Upon success, creates a .ob file and returns SUCCESS.
+ *         Otherwise, an error code is returned.
+ */
+
+result_t AssembleFile(char *file_path);
 /* 
  * For each file in files:
- * 1. preprocess
- * 2. first pass
- * 3. second pass
- * 4. create obj file.
+ * 1. first pass
+ * 2. second pass
+ * 3. create obj file.
  */
 
 #endif /* __ASSEMBLER__ */
