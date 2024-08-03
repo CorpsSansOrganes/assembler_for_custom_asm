edfc39b0bf3c9d0ea71a42abc84e9be67e03e1a4
Eden.Kellner <ednkell@gmail.com>
2024-08-03 11:04:08 +0300

    Fixed list, implemented with a dummy node to avoid corner cases

--- src/list.c
+++ src/list.c
@@ -2,7 +2,7 @@
 #include <stdlib.h> /* malloc, free */
 
 struct node {
-  void *value; // pointer to symbol
+  void *value; 
   node_t *next;
 };
 
@@ -11,73 +11,91 @@ struct list {
   node_t *tail;
 };
 
+static node_t *CreateDummy(void);
+static node_t *CreateNode(node_t *next, void *value);
+static node_t *GetHead(list_t *list);
+
 list_t *CreateList(void) {
-    list_t *new_list = (list_t *)malloc(sizeof(list_t));
-    if (NULL == new_list) {
-        return NULL;
-    }
+  list_t *new_list = (list_t *)malloc(sizeof(list_t));
+  node_t *dummy_node = NULL;
 
-    new_list->head = NULL;
-    new_list->tail = NULL;
+  if (NULL == new_list) {
+    return NULL;
+  }
 
-    return new_list;
+  dummy_node = CreateDummy();
+  if (NULL == dummy_node) {
+    free(new_list);
+    return NULL;
+  }
+
+  new_list->head = dummy_node;
+  new_list->tail = dummy_node;
+
+  return new_list;
 }
 
-node_t *AddNode(list_t *list, void *value){
-   node_t *new_node =  (node_t *)malloc(sizeof(node_t));
+node_t *AddNode(list_t *list, void *value) {
+  node_t *new_node = CreateNode(NULL, value);
 
-   if (NULL == new_node) {
-        return NULL;
-   }
+  if (NULL == new_node) {
+    return NULL;
+  }
 
-   new_node->value = value;
-   list->tail->next = new_node;
-   list->tail = new_node;
-   new_node->next = NULL;
-   return new_node;
+  list->tail->next = new_node;
+  list->tail = new_node;
+  return new_node;
 }
 
-node_t* Find(list_t *list, cmp_func func, void *key){
-   node_t *pointer = list->head;
+node_t* Find(list_t *list, cmp_func func, void *key) {
+  node_t *cur_node = GetHead(list);
 
-   while (NULL != pointer ) {
-    if (func (pointer->value, key)) {
-      return pointer;
+  while (NULL != cur_node) {
+    if (1 == func(cur_node->value, key)) {
+      return cur_node;
     }
-    pointer = pointer->next;
+    cur_node = cur_node->next;
   }
 
   return NULL;
 }
 
 void DestroyList(list_t *list) {
-    if (NULL == list) {
-        return; 
-    }
+  node_t *cur_node = list->head;
+  node_t *next_node = NULL;
 
-    node_t *current = list->head;
-    node_t *next_node;
+  while (NULL != cur_node) {
+    next_node = cur_node->next;    
+    free(cur_node);
+    cur_node = next_node;
+  }
 
-
-    while (NULL != current ) {
-        next_node = current->next;
-        free(current); 
-        current = next_node; 
-    }
-
-    free(list);
+  free(list);
 }
 
 node_t *GetNext(node_t *node) {
-  if (NULL == node) {
-    return NULL;
-  }	    
   return node->next;
 }
 
 void *GetValue(node_t *node) {
+  return node->value;
+}
+
+static node_t *CreateDummy(void) {
+  return CreateNode(NULL, (void *)(0xDEADBEEF));
+}
+
+static node_t *CreateNode(node_t *next, void *value) {
+  node_t *node = (node_t *)malloc(sizeof(node_t));
   if (NULL == node) {
     return NULL;
-  }	    
-  return node->value;
+  }
+
+  node->next = next;
+  node->value = value;
+  return node;
+}
+
+static node_t *GetHead(list_t *list) {
+  return list->head->next; /* list->head is dummy */
 }
