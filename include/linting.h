18c60011eb51ceaacc4974599eb61fe487341537
Eden.Kellner <ednkell@gmail.com>
2024-08-04 14:29:04 +0300

    LintFile API change

--- include/linting.h
+++ include/linting.h
@@ -1,14 +1,19 @@
 #ifndef __LINTING__
 #define __LINTING__
 
+#include "utils.h"
+
 /*
  * @brief Cosmetic touch-up of an input file.
  *        1. Remove comments.
  *        2. Remove blank lines.
  *        3. Remove extraneous spaces, tabs, etc.
  *
- *  The input file is modified with those changes.
+ * @param input_path - Path to the input file.
+ *        output_path - Path to the output file.
+ *
+ * @return SUCCESS upon success, or an appropriate error code.
  */
-void LintFile(char *file_path);
+result_t LintFile(char *input_path, char *output_path);
 
 #endif /* __LINTING__ */
