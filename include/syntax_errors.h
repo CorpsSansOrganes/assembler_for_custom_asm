5f627bd917a9107da37cb38274300ec6478af8af
shalev goldberg <shaleveng@gmail.com>
2024-08-08 12:58:27 +0300

    print errors

--- include/syntax_errors.h
+++ include/syntax_errors.h
@@ -7,33 +7,16 @@
 #include "list.h"
 #include "preprocessing.h"
 
-/* 
-* An enum containing all of the error messages
+typedef struct {
+  unsigned int line_number;
+  bool_t verbose;
+  const char *file_name;
+} error_check_info_t;
+/*
+// ~~--~~--~~--~~--~~
+// Preprocessor errors
+// ~~--~~--~~--~~--~~
 */
-typedef enum {
-  VALID_SYNTAX,
-  RESERVED_NAME_ERROR,
-  EXTRA_CHARACTERS_MACRO_ERROR,
-  INSTRUCTION_NOT_EXIST_ERROR,
-  NOT_ENOUGH_OPERATORS_ERROR,
-  TOO_MANY_OPERATORS_ERROR,
-  WRONG_ADDRESSING_METHOD_ERROR,
-  REGISTER_NAME_NOT_EXIST_ERROR,
-  SYMBOL_ALREADY_DEFINED_ERROR,
-  WRONG_COLON_SYNTAX_ERROR,
-  LABEL_EXCEEDED_LENGTH_ERROR,
-  LABEL_PREFIX_ERROR,
-  ILLEGAL_LABEL_CHARACTERS_ERROR,
-  DIRECTIVE_NOT_EXIST_ERROR,
-  UPPERCASE_DIRECTIVE_ERROR,
-  MISSING_COMMA_ERROR,
-  SYMBOL_NOT_EXIST_ERROR
-} syntax_error_code_t;
-
-
-/* ~~--~~--~~--~~--~~
-  Preprocessor errors
-  ~~--~~--~~--~~--~~ */
 
 /*
  * @brief Checks if there are any characters after a certain point
@@ -41,48 +24,50 @@ typedef enum {
  *
  * @param starting_from - Pointer to the point from which we check for
  *   extrenuous characters.
+ *   verbose - determines whether to print an error or not.
  *
  * @return TRUE if extra characters were detected, FALSE otherwise.
  */
-
-bool_t DetectExtraCharacters(const char *starting_from);
+bool_t DetectExtraCharacters(const char *starting_from, error_check_info_t error_check_info);
 
 /*
  * @brief Checks if name is reserved by the language.
  *
  * @param name - The name to check.
+ *        verbose - determines whether to print an error or not.
  *
  * @return TRUE if is a reserved name, FALSE otherwise.
  */
 
-bool_t IsReservedName(char *name);
+bool_t IsReservedName(char *name, error_check_info_t error_check_info);
 
-
-/* ~~--~~--~~--~~--~~
-  Instruction errors
-  ~~--~~--~~--~~--~~ */
+/*
+// ~~--~~--~~--~~--~~
+// Instruction errors
+// ~~--~~--~~--~~--~~
+*/
 
 /*
  * @brief Checks if an instruction exist.
  *
  * @param instruction - The suspected instruction.
+ *        verbose - determines whether to print an error or not.
  *
  * @return TRUE if the instruction exists, FALSE otherwise.
  */
-
-bool_t DoesInstructionExist(char *instruction);
+bool_t DoesInstructionExist(char *instruction, error_check_info_t error_check_info);
 
 /*
  * @brief Check if number of operands is correct for a given instruction line.
  *
  * @param instruction - The operator
  *        num_of_operands - Number of operands handed to the operator.
+ *        verbose - determines whether to print an error or not.
  * 
  * @return the number of operands minus the number of needed operands.
  *         thus, return 0 if it fits. 
  */
-
-int WrongNumberOfOperands(char *instruction, int num_of_operands);
+int WrongNumberOfOperands(char *instruction, int num_of_operands, error_check_info_t error_check_info);
 
 /*
  * @brief Tell if an operand addressing method is viable for a given instruction.
@@ -90,169 +75,181 @@ int WrongNumberOfOperands(char *instruction, int num_of_operands);
  * @param instruction - The instruction performed.
  *        operand - The operand we check (e.g. '5', 'r0').
  *        type - Destination or source operand.
+ *        verbose - determines whether to print an error or not.
  *
  * @return TRUE if the addressing method of the operand is illegal in the given
  *   instruction, or FALSE otherwise.
  */
-
 bool_t IncorrectAddressingMethod(char *instruction,
                                  char *operand,
-                                 operand_type_t type);
+                                 operand_type_t type, error_check_info_t error_check_info);
 
-
-/* ~~--~~--~~--~~--~~
-  Symbol (label) errors
-  ~~--~~--~~--~~--~~ */
+/*
+// ~~--~~--~~--~~--~~
+// Symbol (label) errors
+// ~~--~~--~~--~~--~~
+*/
 
 /*
  * @briefTell if theres been an attempt to define a symbol that have been defined previously 
  *
  * @param symbol - The symbol which now defined
  *        table - The symbol table.
+ *        verbose - determines whether to print an error or not.
  *
  * @return TRUE if the symbol has already been defined, or FALSE otherwise.
  */
-
-bool_t SymbolDefinedMoreThanOnce(char *symbol, symbol_table_t *table);
-
+bool_t SymbolDefinedMoreThanOnce(char *symbol, symbol_table_t *table, error_check_info_t error_check_info);
 /*
  * @brief Tell if a symbol was used without colon or with space between the symbol and the colon
  *
-  * @param symbol - The symbol which now been called
+ * @param symbol - The symbol which now been called
+ *        verbose - determines whether to print an error or not.
  *
  * @return TRUE if a symbol was used without colon or with space between the symbol and the colon, or FALSE otherwise.
  */
 
-bool_t ColonSyntaxError(char *symbol);
+bool_t ColonSyntaxError(char *symbol, error_check_info_t error_check_info);
 
 /*
  * @brief Tell if a theres been an attempt to use a symbol that havent been defined previously
  *
   * @param symbol - The symbol which now been called
  *         table - The symbol table.
+ *         verbose - determines whether to print an error or not.
  *
  * @return TRUE if the symbol never been defined previously, or FALSE otherwise.
  */
 
-bool_t SymbolWasntDefined(char *symbol, symbol_table_t *table);
+
+bool_t SymbolWasntDefined(char *symbol, symbol_table_t *table, error_check_info_t error_check_info);
 
 /*
  * @brief Tell if a symbol that been directed as .extern was already defined as .entry
  *
- * @param symbol - The symbol that been checked
+ * @param symbol - The symbol that been checked.
  *         table - The symbol table.
+ *         verbose - determines whether to print an error or not.
  *
  * @return TRUE if the symbol was already defined as .entry, or FALSE otherwise.
  */
 
-bool_t SymbolAlreadyDefinedAsEntry(char *symbol, symbol_table_t *table);
+bool_t SymbolAlreadyDefinedAsEntry(char *symbol, symbol_table_t *table, error_check_info_t error_check_info);
 
 /*
  * @brief Tell if a symbol that been directed as .entry was already defined as .extern
  *
- * @param symbol - The symbol that been checked
+ * @param symbol - The symbol that been checked.
  *         table - The symbol table.
+ *         verbose - determines whether to print an error or not.
  *
  * @return TRUE if the symbol was already defined as .extern, or FALSE otherwise.
  */
 
-bool_t SymbolAlreadyDefinedAsExtern(char *symbol, symbol_table_t *table);
+bool_t SymbolAlreadyDefinedAsExtern(char *symbol, symbol_table_t *table, error_check_info_t error_check_info);
 
 /*
  * @brief Tell if a symbol has illegal characters in it (must be alphabetical or numerical characters only)
  *
   * @param symbol - The symbol (without the colon at the end).
+  *        verbose - determines whether to print an error or not.
  *
  * @return TRUE if the symbol is illegal, or FALSE otherwise.
  */
 
-bool_t SymbolIsIllegal(char *symbol);
+bool_t SymbolIsIllegal(char *symbol, error_check_info_t error_check_info);
 
 /*
  * @brief Tell if a symbol begins with illegal characters (must begin with alphabetical characters only)
  *
-  * @param symbol - The symbol.
+ * @param symbol - The symbol.
+ *        verbose - determines whether to print an error or not.
  *
  * @return TRUE if the symbol is illegal, or FALSE otherwise.
  */
 
-bool_t SymbolPrefixIllegal(char *symbol);
+bool_t SymbolPrefixIllegal(char *symbol, error_check_info_t error_check_info);
 
 /*
  * @brief Tell if the length of the symbol name exceeded the character limit (31).
  *
-  * @param symbol - The symbol.
+ * @param symbol - The symbol.
+ *        verbose - determines whether to print an error or not.
  *
  * @return TRUE if the symbol exceeded the character limit, or FALSE otherwise.
  */
 
-bool_t SymbolExceedCharacterLimit(char *symbol);
+bool_t SymbolExceedCharacterLimit(char *symbol, error_check_info_t error_check_info);
 
 /*
  * @brief Tell if a symbol name already used as a macro.
  *
   * @param symbol - The symbol 
  *         macro_list - The list of macros.
+ *         verbose - determines whether to print an error or not.
  *
  * @return TRUE if the symbol name already used as a macro, or FALSE otherwise.
  */
 
-bool_t SymbolUsedAsAMacro(char *symbol, macro_table_t *macro_list);
+bool_t SymbolUsedAsAMacro(char *symbol, macro_table_t *macro_list, error_check_info_t error_check_info);
 
+// ~~--~~--~~--~~--~~
+// Directive errors
+// ~~--~~--~~--~~--~~
 
-/* ~~--~~--~~--~~--~~
-  Directive errors
-  ~~--~~--~~--~~--~~ */
 
 /*
  * @brief Tell if the directive that been called does not exist (directive always starts with '.'). 
  * the existing directive are : .data , .string , .entry , .extern
  *
-  * @param directive - The directive that have been called.
+ * @param directive - The directive that have been called.
+ *        verbose - determines whether to print an error or not.
  *
- * @return TRUE if the dorective doesn't exist, or FALSE otherwise.
+ * @return TRUE if the directive doesn't exist, or FALSE otherwise.
  */
 
-bool_t DirectiveDoesntExist(char *directive);
-
+bool_t DirectiveDoesntExist(char *directive, error_check_info_t error_check_info);
 /*
  * @brief Tell if the directive is exist but have been called with uppercase letters instead of lowercase letters
  *
-  * @param directive - The directive that have been called.
+ * @param directive - The directive that have been called.
+ *        verbose - determines whether to print an error or not.
  *
  * @return TRUE if the directive have been called with uppercase letters , or FALSE otherwise.
  */
 
-bool_t DirectiveIsUpperCase(char *directive);
+bool_t DirectiveIsUpperCase(char *directive, error_check_info_t error_check_info);
 
 /*
  * @brief Tell if the .data directive line that been performed is missing commas
- *        between the parameters. 
+ *        between the parameters. assuming the line is linted
  *
  * @param data - The .data directive line that have been performed (starting 
  *        from the first parameter, i.e not including the word '.data ' itself).
+ *        verbose - determines whether to print an error or not.
  *
  * @return TRUE if the .data directive line is missing commas between the parameters,
  *         or FALSE otherwise.
  */
 
-bool_t CommaIsMissingInData(char *data);
+bool_t CommaIsMissingInData(char *data, error_check_info_t error_check_info);
 
 
-/* ~~--~~--~~--~~--~~
-  General syntax
-  ~~--~~--~~--~~--~~ */
+// ~~--~~--~~--~~--~~
+// General syntax
+// ~~--~~--~~--~~--~~
 
 /*
  * @brief Tell if a register name doesn't exist.
  * the register names that exist are (r0 ... r7)
  *
-  * @param register_name - The name of the register that been called
+ * @param register_name - The name of the register that been called.
+ *        verbose - determines whether to print an error or not.
  *
  * @return TRUE if a register name doesn't exist, or FALSE otherwise.
  */
 
-bool_t RegisterNameDoesntExist(char *register_name);
+bool_t RegisterNameDoesntExist(char *register_name, error_check_info_t error_check_info);
 
 #endif /* __SYNTAX_ERRORS__ */
 
--- src/syntax_errors.c
+++ src/syntax_errors.c
@@ -15,13 +15,16 @@ static addressing_methods detect_addressing_method (char *operand);
 static int SymbolCompare (void *str,void *key );
 
 
-bool_t DetectExtraCharacters(const char *starting_from) {
+bool_t DetectExtraCharacters(const char *starting_from, error_check_info_t error_check_info) {
   const char blank_chars[] = {' ', '\t'};
   char *ptr = (char *)starting_from;
   bool_t result = FALSE;
 
   while ('\0' != *ptr) {
     if (strchr(blank_chars, *ptr)) {
+      if (error_check_info.verbose){
+        printf ("Error: extrenuous characters detected at line %u in the file: %s", error_check_info.line_number, error_check_info.file_name);
+      }
       result = TRUE;
       break;
     }
@@ -31,42 +34,56 @@ bool_t DetectExtraCharacters(const char *starting_from) {
   return result;
 }
 
-bool_t SymbolDefinedMoreThanOnce(char *symbol, symbol_table_t *table){
+bool_t SymbolDefinedMoreThanOnce(char *symbol, symbol_table_t *table, error_check_info_t error_check_info){
    int is_defined = (int) FindSymbol (table, symbol);
    if (is_defined != -1){
-	return TRUE;
+	if (error_check_info.verbose){
+        printf ("Error: attempted to define a symbol at line %u in the file: %s that already been defined", error_check_info.line_number, error_check_info.file_name);
+    }
+    return TRUE;
    }
    return FALSE;
 }
 
-bool_t SymbolWasntDefined(char *symbol, symbol_table_t *table){
+bool_t SymbolWasntDefined(char *symbol, symbol_table_t *table, error_check_info_t error_check_info){
    int is_defined = (int) FindSymbol (table, symbol);
    if (is_defined == -1){
 	return FALSE;
    }
+    if (error_check_info.verbose){
+        printf ("Error: attempted to call a symbol at line %u in the file: %s that wasn't defined", error_check_info.line_number, error_check_info.file_name);
+    }
    return TRUE;
 }
 
-bool_t ColonSyntaxError(char *symbol){
+bool_t ColonSyntaxError(char *symbol, error_check_info_t error_check_info){
     while (*(symbol+1) != '\0'){
        symbol++;
     }
     if (*symbol != ':'){
+        if (error_check_info.verbose){
+        printf ("Error: symbol was used without colon or with space between the symbol and the colon at line %u in the file: %s", error_check_info.line_number, error_check_info.file_name);
+        }
 	return TRUE;
     }
     return FALSE;
 }
 
-int WrongNumberOfOperands(char *instruction, int num_of_operands){
+int WrongNumberOfOperands(char *instruction, int num_of_operands, error_check_info_t error_check_info){
     for (int i=0; i<NUM_OF_INSTRUCTIONS;i++){
         if (address_method_table[i].name == *instruction)
         {
-            return num_of_operands - (address_method_table[i].is_there_destination_operand+address_method_table[i].is_there_source_operand);
+            int required_operands = (address_method_table[i].is_there_destination_operand+address_method_table[i].is_there_source_operand);
+            if (error_check_info.verbose){
+                printf ("Error: required %d operands, but given %d. at line %u in the file: %s", required_operands, num_of_operands, error_check_info.line_number, error_check_info.file_name);
+            }
+            return num_of_operands-required_operands;        
         }
     }
 }
 
-bool_t IncorrectAddressingMethod(char *instruction, char *operand, operand_type_t type){
+/*to add print error!*/
+bool_t IncorrectAddressingMethod(char *instruction, char *operand, operand_type_t type, error_check_info_t error_check_info){
     int method = (int) detect_addressing_method (operand);
         for (int i=0; i<NUM_OF_INSTRUCTIONS;i++){
             if (address_method_table[i].name == *instruction){
@@ -81,29 +98,39 @@ bool_t IncorrectAddressingMethod(char *instruction, char *operand, operand_type_
     
 }
 
-bool_t SymbolAlreadyDefinedAsEntry(char *symbol_name, symbol_table_t *table){
+/*to add print error!*/
+bool_t SymbolAlreadyDefinedAsEntry(char *symbol_name, symbol_table_t *table, error_check_info_t error_check_info){
     symbol_t *extern_symbol = FindSymbol(table,symbol_name);
     return (GetSymbolType(symbol_name) == ENTRY);
 }
 
-bool_t SymbolAlreadyDefinedAsExtern(char *symbol_name, symbol_table_t *table){
+/*to add print error!*/
+
+bool_t SymbolAlreadyDefinedAsExtern(char *symbol_name, symbol_table_t *table, error_check_info_t error_check_info){
     symbol_t *entry_symbol = FindSymbol(table,symbol_name);
     return (GetSymbolType(symbol_name) == EXTERN);
 }
 
-bool_t DoesInstructionExist(char *instruction){
+bool_t InstructionDoesntExist(char *instruction, error_check_info_t error_check_info){
    for (int i = 0; i < NUM_OF_INSTRUCTIONS; i++) {
        if (strcmp(instruction, reserved_instructions[i]) == 0) {
-           return TRUE;
+           return FALSE;
        }
     }
+    if (error_check_info.verbose){
+        printf ("ERROR: the instruction at line %u in the file: %s doesn't exist", error_check_info.line_number, error_check_info.file_name);
+    }
+    return TRUE;
 }
 
-bool_t SymbolIsIllegal(char *symbol) {
+bool_t SymbolIsIllegal(char *symbol, error_check_info_t error_check_info) {
 
     for (int i = 0; i < strlen(symbol); i++)
      {
         if (!((*symbol >= 'A' && *symbol <= 'Z') || (*symbol >= 'a' && *symbol <= 'z') || (*symbol >= '0' && *symbol <= '9'))) {
+            if (error_check_info.verbose){
+                printf ("ERROR: use of illegal characters in the symbol name at line %u in the file: %s", error_check_info.line_number, error_check_info.file_name);
+            }
             return TRUE;
         }
         symbol++;
@@ -112,49 +139,63 @@ bool_t SymbolIsIllegal(char *symbol) {
     return FALSE;
 }
 
-bool_t SymbolPrefixIllegal(char *symbol) {
+bool_t SymbolPrefixIllegal(char *symbol, error_check_info_t error_check_info) {
 
     // Check if the first character is not an alphabetical character
     if (!((*symbol >= 'A' && *symbol <= 'Z') || (*symbol >= 'a' && *symbol <= 'z'))) {
+        if (error_check_info.verbose){
+            printf ("ERROR: illegal character in the prefix of a symbol at line %u in the file: %s", error_check_info.line_number, error_check_info.file_name);
+        }
         return TRUE;
     }
 
     return FALSE;
 }
 
-bool_t IsReservedName(char *name) {
+bool_t IsReservedName(char *name, error_check_info_t error_check_info) {
 
     for (int i = 0; i < NUM_OF_INSTRUCTIONS; i++) {
         if (strcmp(name, reserved_instructions[i]) == 0) {
+            if (error_check_info.verbose){
+                 printf ("ERROR: attempt to make use of a reserved instruction name at line %u in the file: %s ", error_check_info.line_number, error_check_info.file_name);
+            }
             return TRUE;
         }
     }
     for (int i = 0; i < NUM_OF_DIRECTIVES; i++) {
         if (strcmp(name, reserved_directives[i]) == 0) {
+            if (error_check_info.verbose){
+                printf ("ERROR: attempt to make use of a reserved directive name at line %u in the file: %s ", error_check_info.line_number, error_check_info.file_name);
+            }
             return TRUE;
         }
 
     }
     for (int i = 0; i < NUM_OF_REGISTERS; i++) {
         if (strcmp(name, register_names[i]) == 0) {
+            if (error_check_info.verbose){
+                printf ("ERROR: attempt to make use of a reserved register name at line %u in the file: %s ", error_check_info.line_number, error_check_info.file_name);
+            }
             return TRUE;
         }
     }
     return FALSE;
 }
 
-bool_t RegisterNameDoesntExist(char *register_name){
+bool_t RegisterNameDoesntExist(char *register_name, error_check_info_t error_check_info){
 
     for (int i = 0; i < NUM_OF_REGISTERS; i++) {
         if (strcmp(register_name, register_names[i]) == 0) {
             return FALSE;
         }
     }
-
+    if (error_check_info.verbose){
+        printf ("ERROR: the register name at line %u in the file: %s doesn't exist", error_check_info.line_number, error_check_info.file_name);
+    }
     return TRUE;
 }
 
-bool_t DirectiveDoesntExist(char *directive) {
+bool_t DirectiveDoesntExist(char *directive, error_check_info_t error_check_info) {
 
 
     for (int i = 0; i < NUM_OF_DIRECTIVES; i++) {
@@ -162,18 +203,23 @@ bool_t DirectiveDoesntExist(char *directive) {
             return FALSE;
         }
     }
-
+    if (error_check_info.verbose){
+        printf ("ERROR: the directive name at line %u in the file: %s doesn't exist", error_check_info.line_number, error_check_info.file_name);
+    }
     return TRUE;
 }
-bool_t DirectiveIsUpperCase(char *directive){
-    if (DirectiveDoesntExist(directive) && !DirectiveDoesntExist(tolower(directive)))
+bool_t DirectiveIsUpperCase(char *directive, error_check_info_t error_check_info){
+    if (DirectiveDoesntExist(directive, verbose) && !DirectiveDoesntExist(tolower(directive), verbose))
     {
+        if (error_check_info.verbose){
+            printf ("ERROR: directive was written in uppercase letters instead of lowercase letters at line: %u in the file: %s doesn't exist", error_check_info.line_number, error_check_info.file_name);
+        }
         return TRUE;
     }
     return FALSE;
 }
 
-bool_t SymbolExceedCharacterLimit(char *symbol) {
+bool_t SymbolExceedCharacterLimit(char *symbol, error_check_info_t error_check_info) {
     if (symbol == NULL) {
         return FALSE;
     }
@@ -182,6 +228,9 @@ bool_t SymbolExceedCharacterLimit(char *symbol) {
     while (*symbol != '\0') {
         length++;
         if (length > 31) {
+            if (error_check_info.verbose){
+                printf ("ERROR: the name of the symbol at line %u in the file: %s exceeded the character limit", error_check_info.line_number, error_check_info.file_name);
+            }
             return TRUE;
         }
         symbol++;
@@ -189,18 +238,24 @@ bool_t SymbolExceedCharacterLimit(char *symbol) {
 
     return FALSE;
 }
-bool_t SymbolUsedAsAMacro(char *symbol, macro_table_t *macro_list){
+bool_t SymbolUsedAsAMacro(char *symbol, macro_table_t *macro_list, error_check_info_t error_check_info){
    if (NULL != Find (symbol, SymbolCompare, macro_list)){
-	return TRUE;
+	if (error_check_info.verbose){
+        printf ("ERROR: attempt to define a symbol name that is already define as a macro at line %u in the file: %s", error_check_info.line_number, error_check_info.file_name);
+    }
+    return TRUE;
    }  
    return FALSE;
 }
 
-bool_t CommaIsMissingInData(char *data){
+bool_t CommaIsMissingInData(char *data, error_check_info_t error_check_info){
    while (*data != '\0') {
 	 if (*data == ' ' || *data == '\t' ){
 		if (*(data-1) != ','){
-			return TRUE;
+			if (error_check_info.verbose){
+                printf ("ERROR: a comma is missing between parameters at line %u in the file: %s", error_check_info.line_number, error_check_info.file_name);
+            }
+            return TRUE;
         }
      }
    }
@@ -231,12 +286,12 @@ static addressing_methods detect_addressing_method (char *operand){
     }
         return DIRECT;
     if (*operand == '*'){
-        if (RegisterNameDoesntExist(*(operand+1))){
+        if (RegisterNameDoesntExist(*(operand+1),FALSE)){
             return INVALID;
         }
         return INDIRECT_REGISTER;
     }
-    if (!RegisterNameDoesntExist(*operand)){
+    if (!RegisterNameDoesntExist(*operand,FALSE)){
         return DIRECT_REGISTER;
     }
     return DIRECT;
