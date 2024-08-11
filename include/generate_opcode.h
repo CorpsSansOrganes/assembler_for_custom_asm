ba7576503a598c0e0a3be43cfd6725b43867e4df
shalev goldberg <shaleveng@gmail.com>
2024-08-11 17:14:24 +0300

    instruction handling almost finished

--- include/generate_opcode.h
+++ include/generate_opcode.h
@@ -1,4 +1,15 @@
+#include "assembler.h"
+#include "utils.h"
+#include "syntax_errors.h"
+#include "macro_table.h"
+#include "symbol_table.h"
+#include <stdio.h> /* perror */
+#include <string.h> 
+#include <stdlib.h>
+
+
 typedef struct {
   /* 1-3 words */
-  bitmap_t opcode;
-} instruction_t;
+  bitmap_t *opcode;
+  size_t size;
+} line_opcode_t;
